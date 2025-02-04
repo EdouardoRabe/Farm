a1f4d9e2cf9fd3703cd52f11f34f0d0e815baa21
EdouardoRabe <edouardorabendrazana@gmail.com>
2025-02-04 11:59:24 +0300

    fix: achatController and else if condition

--- app/controllers/AchatController.php
+++ app/controllers/AchatController.php
@@ -26,29 +26,29 @@ class AchatController {
         echo $result = $gestionModel->calculerCapital($_POST['date_achat'],$_SESSION['id_user']);echo '</br>';
         echo $_POST['prix_achat_kg']*$_POST['poids_initial'];
         if($result<($_POST['prix_achat_kg']*$_POST['poids_initial'])){
-            Flight::redirect('tableAchat?error');
-        }
-        $reponse=[
-            "id_typeAnimal"=> $_POST['id_typeAnimal'],
-            'poids_initial'=> $_POST['poids_initial']
-        ];
-        $insertAnimal=$generelaiserModel-> insererDonnee('ferme_animal',$reponse,'POST');
-        $animal_id=$generelaiserModel-> getLastInsertedId('ferme_animal','id_animal');
-        $donnee=[
-            'id_animal'=>$animal_id['last_id'],
-            'date_achat'=>$_POST['date_achat'],
-            'id_user'=>$_SESSION['id_user'],
-            'montant'=>$_POST['prix_achat_kg']*$_POST['poids_initial'],
-        ];
-       
-         $insertAchat=$generelaiserModel-> insererDonnee('ferme_achat_animal',$donnee,'POST');
-
-        if ($insertAchat) {
-            $result = $gestionModel->calculerCapital(date("Y-m-d H:i:s"),$_SESSION['id_user']);
-            $_SESSION['result']=$result;
-            Flight::redirect('tableAchat?success');
-        } else {
             Flight::redirect('tableAchat?error=1');
+        }
+        else{
+            $reponse=[
+                "id_typeAnimal"=> $_POST['id_typeAnimal'],
+                'poids_initial'=> $_POST['poids_initial']
+            ];
+            $insertAnimal=$generelaiserModel-> insererDonnee('ferme_animal',$reponse,'POST');
+            $animal_id=$generelaiserModel-> getLastInsertedId('ferme_animal','id_animal');
+            $donnee=[
+                'id_animal'=>$animal_id['last_id'],
+                'date_achat'=>$_POST['date_achat'],
+                'id_user'=>$_SESSION['id_user'],
+                'montant'=>$_POST['prix_achat_kg']*$_POST['poids_initial'],
+            ];
+            $insertAchat=$generelaiserModel-> insererDonnee('ferme_achat_animal',$donnee,'POST');
+            if ($insertAchat) {
+                $result = $gestionModel->calculerCapital(date("Y-m-d H:i:s"),$_SESSION['id_user']);
+                $_SESSION['result']=$result;
+                Flight::redirect('tableAchat?success');
+            } else {
+                Flight::redirect('tableAchat?error=1');
+            }
         }
     }
 
